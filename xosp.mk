5e54879abd0160365986271124618f0a3548805a
matteo0026 <marmotta200026@gmail.com>
2016-12-20 22:02:15 +0100

    Make D802 sources for XOSP

---
+++ xosp.mk
@@ -0,0 +1,21 @@
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d802/d802.mk)
+
+# Inherit some propietary XOSP stuff
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d802
+PRODUCT_NAME := xosp_d802
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D802
+PRODUCT_MANUFACTURER := lge
+RELEASE_TYPE := XOSP_OFFICIAL
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=lge/g2_open_com/g2:5.0.2/LRX22G/151061918340a:user/release-keys \
+    PRIVATE_BUILD_DESC="g2_open_com-user 5.0.2 LRX22G 151061918340a release-keys"
+
